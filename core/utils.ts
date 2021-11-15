7c49d2d42141e6f0000bb92eff60d2587a5ba05c
John Livee Oroncillo <johnliveeoroncillo@gmail.com>
2021-11-15 22:37:49 +0800

    update

--- core/utils.ts
+++ core/utils.ts
@@ -1,5 +1,13 @@
 
 const bcrypt = require('bcrypt');
+import Crypto from 'crypto-js';
+require('dotenv').config();
+
+const vector_key: any = process.env.VECTOR_KEY;
+const secret_key: any = process.env.SECRET_KEY;
+const salt: any = { iv : vector_key };
+
+
 const timestamp = (date: Date): number => {
     var datum = Date.parse(date.toString());
     return datum / 1000;
@@ -15,12 +23,31 @@ const cryptPassword = async (password:string): Promise<string> => {
     return bcrypt.hash(password, 10).then((hash:string) =>  {
         return hash
     });
- };
+};
  
- const comparePassword = async (plainPass:string, hashword:string): Promise<boolean> => {
+const comparePassword = async (plainPass:string, hashword:string): Promise<boolean> => {
     return bcrypt.compare(plainPass, hashword).then(function(result: boolean) {
         return result;
     });
- };
+};
+
+const encrypt = (value: any): string => {
+    if(typeof value === 'object') value = JSON.stringify(value);
+    const encrypted = Crypto.AES.encrypt(value, secret_key, salt).toString()
+    return encrypted;
+}
+
+const decrypt = (value: string): any => {
+    try  {
+        let decrypted: any = Crypto.AES.decrypt(value, secret_key, salt).toString(Crypto.enc.Utf8);
+        if(typeof decrypted === 'object') decrypted = JSON.parse(decrypted);
+        if(decrypted === '') decrypted = null;
+        return decrypted;
+
+    }
+    catch(e) {
+        return null;
+    }
+}
   
-export { timestamp, cryptPassword, comparePassword, date_diff }
+export { encrypt, decrypt, timestamp, cryptPassword, comparePassword, date_diff }
