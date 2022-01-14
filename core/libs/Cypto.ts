df7aef9cc2e364460bc39a33319716176e0bbe4c
John Livee Oroncillo <johnliveeoroncillo@gmail.com>
2022-01-14 21:12:58 +0800

    Adde crypto

---
+++ core/libs/Cypto.ts
@@ -0,0 +1,20 @@
+import CryptoJS from "crypto-js";
+import 'dotenv/config';
+
+export class Crypto {
+    private static key = process.env?.SECRET_KEY ?? '';
+    private static salt:any = CryptoJS.lib.WordArray.random(128/8);
+    private static iv = CryptoJS.enc.Hex.parse(this.salt);
+    
+    static encrypt(plain_text: string): string {
+        return CryptoJS.AES.encrypt(plain_text, this.key, {
+            iv: this.iv,
+          }).toString();
+    }
+
+    static decrypt(encrypted: string): string {
+        return CryptoJS.AES.decrypt(encrypted, this.key, {
+            iv: this.iv,
+          }).toString(CryptoJS.enc.Utf8);
+    }
+}
