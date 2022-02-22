a4820b2d4a49410fdfbec633bbcbd8d73004ef0e
John Livee Oroncillo <zjgoroncillo@917ventures.com>
2022-02-22 15:19:38 +0800

    Redis update

--- core/databases/Redis.ts
+++ core/databases/Redis.ts
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const predis = require('predis');
+const redis = require('predis');
+import 'dotenv/config';
 
 const REDIS_PORT = process.env.REDIS_PORT ?? '6379';
 const REDIS_HOST = process.env.REDIS_HOST ?? '127.0.0.1';
+const REDIS_USERNAME = process.env.REDIS_USERNAME ?? '';
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD ?? '';
 const TTL = process.env.REDIS_TTL ?? 3600;
 
 export class Redis {
     protected client: any;
 
     constructor() {
-        this.client = predis.createClient({
+        this.client = redis.createClient({
             port: REDIS_PORT,
             server: REDIS_HOST,
+            password: REDIS_PASSWORD,
+            username: REDIS_USERNAME,
         });
     }
 
