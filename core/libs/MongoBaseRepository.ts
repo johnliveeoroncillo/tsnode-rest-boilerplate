a66efc4e0de4cdaef1002785af66cb54d50414a5
JL <67359294+johnliveeoroncillo@users.noreply.github.com>
2023-07-20 06:32:21 +0000

    fix(R.6.3.0): adjusted mongodb typings

--- core/libs/MongoBaseRepository.ts
+++ core/libs/MongoBaseRepository.ts
@@ -1,35 +1,40 @@
-import { ObjectId } from 'mongodb';
-import { DeepPartial, ObjectLiteral, Repository, SaveOptions } from 'typeorm';
+import { DeepPartial, ObjectLiteral, Repository, SaveOptions, ObjectID } from 'typeorm';
 import { Carbon } from '../../core/libs/Carbon';
 
 export class MongoBaseRepository<T extends ObjectLiteral> extends Repository<T> {
     //Your generic methods here
-    async findCollections(): Promise<T[]> {
-        const response = await this.find({ withDeleted: true });
+    async findCollections(where?: Record<string, any>): Promise<T[]> {
+        const response = await this.find({ where, withDeleted: true });
         return response.filter((item: T) => !item.deleted_at);
     }
     async findCollection(_id: string): Promise<T | undefined> {
-        const id: ObjectId = new ObjectId(_id);
-        const response = await this.find({
+        const id: ObjectID = new ObjectID(_id);
+        const response: T[] = await this.findCollections({
             where: {
                 _id: id,
             },
-            withDeleted: true,
         });
         if (!response || !response.length) return undefined;
-        const data: any = response.pop();
-        if (data?.deleted_at) return undefined;
-        if (data) data.id = id;
+        const data: T | undefined = response.pop();
+        if (data) {
+            return {
+                id,
+                ...data,
+            };
+        }
         return data;
     }
     async softDeleteCollection(_id: string): Promise<void> {
-        const collection: any = await this.findCollection(_id);
-        console.log(collection);
+        const collection: T | undefined = await this.findCollection(_id);
         if (collection) {
             await this.updateByIdAndReturn(_id, { deleted_at: Carbon.timestamp() });
         }
     }
-    async updateByIdAndReturn(id: any, partialEntity: DeepPartial<any>, options?: SaveOptions): Promise<T | undefined> {
+    async updateByIdAndReturn(
+        id: any,
+        partialEntity: DeepPartial<Record<string, any>>,
+        options?: SaveOptions,
+    ): Promise<T | undefined> {
         await this.update(id, partialEntity);
         return await this.findCollection(id);
     }
